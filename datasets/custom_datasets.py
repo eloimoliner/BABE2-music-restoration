36c84fe68e2573f619b4df928e1fdfa6ec91e240
Emmanuel Infante <163698214+emmanuelinfante@users.noreply.github.com>
2024-04-21 22:39:11 -0600

    Update custom_datasets.py

--- datasets/custom_datasets.py
+++ datasets/custom_datasets.py
@@ -9,22 +9,13 @@
 
 import os
 import numpy as np
-#import PIL.Image
 import json
 import torch
 import random
 import glob
 import soundfile as sf
 
-#try:
-#    import pyspng
-#except ImportError:
-#    pyspng = None
-
-#----------------------------------------------------------------------------
-# Dataset subclass that loads images recursively from the specified directory
-# or ZIP file.
-class AudioSmallFolderDataset(torch.utils.data.IterableDataset):
+class AudioPairFolderDataset(torch.utils.data.IterableDataset):
     def __init__(self,
         dset_args,
         fs=44100,
@@ -40,14 +31,13 @@ class AudioSmallFolderDataset(torch.utils.data.IterableDataset):
         self.dset_args=dset_args
         path=dset_args.path
         self.sigma_data=dset_args.sigma_data
-        
 
-        filelist=glob.glob(os.path.join(path,"*.wav"))
-        assert len(filelist)>0 , "error in dataloading: empty or nonexistent folder"+path
+        clean_filelist=glob.glob(os.path.join(path,"clean","*.wav"))
+        noisy_filelist=glob.glob(os.path.join(path,"noisy","*.wav"))
+        assert len(clean_filelist)>0 and len(noisy_filelist)>0, "error in dataloading: empty or nonexistent folder"+path
 
-        self.train_samples=filelist
+        self.train_samples=list(zip(clean_filelist, noisy_filelist))
         
-       
         self.seg_len=int(seg_len)
         self.fs=fs
 
@@ -56,11 +46,10 @@ class AudioSmallFolderDataset(torch.utils.data.IterableDataset):
 
         lengths=[]
         for f in self.train_samples:
-            data, samplerate=sf.read(f)
+            data, samplerate=sf.read(f[0])  # assuming clean and noisy files have same length
             length=len(data)
             lengths.append(length/samplerate)
             
-        #Normalize lengths to 1
         lengths=np.array(lengths)
         lengths/=np.sum(lengths)
         assert np.sum(lengths)>0.99 and np.sum(lengths)<1.01, "lengths do not sum to 1"
@@ -69,11 +58,12 @@ class AudioSmallFolderDataset(torch.utils.data.IterableDataset):
 
         if self.overfit:
             file=self.train_samples[0]
-            data, samplerate = sf.read(file)
-            if len(data.shape)>1 :
-                data=np.mean(data,axis=1)
-            self.overfit_sample=data[10*samplerate:60*samplerate] #use only 50s
-
+            clean_data, samplerate = sf.read(file[0])
+            noisy_data, _ = sf.read(file[1])
+            if len(clean_data.shape)>1 :
+                clean_data=np.mean(clean_data,axis=1)
+                noisy_data=np.mean(noisy_data,axis=1)
+            self.overfit_sample=(clean_data[10*samplerate:60*samplerate], noisy_data[10*samplerate:60*samplerate]) #use only 50s
 
         self.running_mean=0
 
@@ -81,20 +71,16 @@ class AudioSmallFolderDataset(torch.utils.data.IterableDataset):
             self.calculate_sigma_data()
 
     def calculate_sigma_data(self):
-        N=300 #hardcoded, but that's ok :=>)
+        N=300
         sigmas_data=[]
         for i in range(N):
-                #num=random.randint(0,len(self.train_samples)-1)
-                #use self.priorities to sample random file
                 num=np.random.choice(len(self.train_samples),p=self.priorities)
-
-                #for file in self.train_samples:  
                 file=self.train_samples[num]
-                data, samplerate = sf.read(file)
+                clean_data, samplerate = sf.read(file[0])
+                noisy_data, _ = sf.read(file[1])
                 assert(samplerate==self.fs, "wrong sampling rate")
-                data_clean=data
-                #Stereo to mono
-                if len(data.shape)>1 :
+                data_clean=clean_data
+                if len(clean_data.shape)>1 :
                     a=np.random.uniform(0,1)
                     data_clean=data_clean[:,0]*a+data_clean[:,1]*(1-a)
         
@@ -106,9 +92,7 @@ class AudioSmallFolderDataset(torch.utils.data.IterableDataset):
                     idx=0
                 segment=data_clean[idx:idx+self.seg_len]
                 segment=segment.astype('float32')
-                #segment/=self.sigma_data
                 std=segment.std(-1)
-                #check if any in std is 0
                 if std<2e-4:
                     raise ValueError("std is 0, skipping")
                 sigmas_data.append(std)
@@ -119,63 +103,43 @@ class AudioSmallFolderDataset(torch.utils.data.IterableDataset):
 
     def __iter__(self):
         if self.overfit:
-           data_clean=self.overfit_sample
+           data_clean=self.overfit_sample[0]
+           data_noisy=self.overfit_sample[1]
         while True:
             try:
                 if not self.overfit:
-                    #num=random.randint(0,len(self.train_samples)-1)
-                    #use self.priorities to sample random file
                     num=np.random.choice(len(self.train_samples),p=self.priorities)
-
-                    #for file in self.train_samples:  
                     file=self.train_samples[num]
-                    data, samplerate = sf.read(file)
+                    clean_data, samplerate = sf.read(file[0])
+                    noisy_data, _ = sf.read(file[1])
                     assert(samplerate==self.fs, "wrong sampling rate")
-                    data_clean=data
-                    #Stereo to mono
-                    if len(data.shape)>1 :
+                    data_clean=clean_data
+                    data_noisy=noisy_data
+                    if len(clean_data.shape)>1 :
                         a=np.random.uniform(0,1)
                         data_clean=data_clean[:,0]*a+data_clean[:,1]*(1-a)
+                        data_noisy=data_noisy[:,0]*a+data_noisy[:,1]*(1-a)
         
-                #normalize
-                #no normalization!!
-                #data_clean=data_clean/np.max(np.abs(data_clean))
-             
-                #framify data clean files
                 num_frames=np.floor(len(data_clean)/self.seg_len) 
                 
-                #if num_frames>4:
-                #get 8 random batches to be a bit faster
                 if not self.overfit:
                     idx=np.random.randint(0,len(data_clean)-self.seg_len)
                 else:
                     idx=0
-                segment=data_clean[idx:idx+self.seg_len]
-                segment=segment.astype('float32')
-                #segment/=self.sigma_data
-                std=segment.std(-1)
-                #check if any in std is 0
+                clean_segment=data_clean[idx:idx+self.seg_len]
+                noisy_segment=data_noisy[idx:idx+self.seg_len]
+                clean_segment=clean_segment.astype('float32')
+                noisy_segment=noisy_segment.astype('float32')
+                std=clean_segment.std(-1)
                 if std<2e-4:
                     raise ValueError("std is 0, skipping")
-                #self.running_mean=0.99*self.running_mean+0.01*std
-                #print(self.running_mean)
-                segment/=self.sigma_data
+                clean_segment/=self.sigma_data
+                noisy_segment/=self.sigma_data
                 if self.dset_args.apply_random_gain:
-                    #apply gain defined in gain_range in dB
-                    segment*=10**(np.random.uniform(self.dset_args.gain_range[0],self.dset_args.gain_range[1])/20)
-                #b=np.mean(np.abs(segment))
-                #segment= (10/(b*np.sqrt(2)))*segment #default rms  of 0.1. Is this scaling correct??
-                    
-                #let's make this shit a bit robust to input scale
-                #scale=np.random.uniform(1.75,2.25)
-                #this way I estimage sigma_data (after pre_emph) to be around 1
-                
-                #segment=10.0**(scale) *segment
-                yield  segment
+                    gain=10**(np.random.uniform(self.dset_args.gain_range[0],self.dset_args.gain_range[1])/20)
+                    clean_segment*=gain
+                    noisy_segment*=gain
+                yield  (clean_segment, noisy_segment)
             except Exception as e:
                 print(e)
                 continue
-            #else:
-            #    pass
-
-
